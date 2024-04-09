fbf33ecb5290ebc88c37621a4aea313a12e1b4fd
Vincenzo <vincenzor.celu@gmail.com>
2024-04-09 11:46:29 -0400

    Rutas acomodadas

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,6 @@ use App\Http\Controllers\V1\GenderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum'); */
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
